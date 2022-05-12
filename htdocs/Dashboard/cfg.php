eb904cdd2a4156cb4d864966519ba0dbb4b594a1
11793759 <mohammad.mazlan.mohd.rafiee@intel.com>
2022-05-12 10:48:24 +0800

    Update cfg.php
    update turnin link

--- htdocs/Dashboard/cfg.php
+++ htdocs/Dashboard/cfg.php
@@ -10,7 +10,8 @@ $cfg["HSD_OPEN_BUGS_LINK"] = "https://hsdes.intel.com/appstore/community/#/10184
 $cfg["HSD_PCRS_LINK"] = "https://hsdes.intel.com/appstore/community/#/101846185?queryId=22012761296";
 $cfg["HSD_OPEN_PCRS_LINK"] = "https://hsdes.intel.com/appstore/community/#/101846185?queryId=22012761296";
 $cfg["HSD_SIGHTINGS_LINK"] = "https://hsdes.intel.com/appstore/community/#/101846185?queryId=14010786778";
-$cfg["TURNIN_PAGE"] = "http://fmygk2025.fm.intel.com:9999/turnins?status=filter&status=integrating&status=holdingpen&status=pending&status=held&status=failed&age=2+Weeks&sort=gk";
+$cfg["TURNIN_PAGE_GK-HDK"] = "http://fmygk2025.fm.intel.com:9999/turnins?status=filter&status=integrating&status=holdingpen&status=pending&status=held&status=failed&age=2+Weeks&sort=gk";
+$cfg["TURNIN_PAGE_GK-CTH2"] = "http://scygatkpr292.zsc11.intel.com:45499/turnins?cluster=ip&stepping=spbc-boot&status=accepted&status=cancelled&status=failed&status=filter&status=held&status=holdingpen&status=integrating&status=pending&status=rejected&status=released&age=2+Weeks&display=table";
 $cfg["QOV_LINK"] = "http://ontrak5.intel.com/IP?formfactor=Desktop&repositoryItemId=e03b09f0-455c-4144-a9d3-875767315dd2";
 $cfg["RUN_COMMANDS_LINK"] = "https://teams.microsoft.com/l/entity/com.microsoft.teamspace.tab.wiki/tab::54addc48-9947-413e-aed4-1e7d651907cb?context=%7b%22subEntityId%22:%22%7b%5c%22pageId%5c%22:2%2c%5c%22sectionId%5c%22:4%2c%5c%22origin%5c%22:2%7d%22%2c%22channelId%22:%2219:c33a0950b07445e2824b0f7736a4cc6a%40thread.tacv2%22%7d&tenantId=46c98d88-e344-4ed4-8496-4ed7712e255d";
 
